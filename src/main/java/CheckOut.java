89a57f48f7fee46dd902a277e509917c45b71f2b
JinalB02 <jinalbafna02@gmail.com>
2022-02-02 17:13:55 +0100

    Add files via upload

--- src/main/java/CheckOut.java
+++ src/main/java/CheckOut.java
@@ -1,13 +1,20 @@
-import service.BillInput;
+import repository.ItemsRepository;
+import service.Bill;
 import service.ProcessBill;
 
 import java.util.ArrayList;
 
 public class CheckOut {
     public static void main(String[] args) {
-        ProcessBill processBill = new ProcessBill();
-        BillInput billInput = new BillInput();
-        ArrayList<Long> products = billInput.addItems();
-        processBill.printBill(products);
+        ItemsRepository itemsRepository = new ItemsRepository();
+        ProcessBill processBill = new ProcessBill(itemsRepository);
+        Bill bill = new Bill();
+        bill.addItem(Long.parseLong("01001"));
+        bill.addItem(Long.parseLong("02002"));
+        bill.addItem(Long.parseLong("03003"));
+        bill.addItem(Long.parseLong("03003"));
+        bill.addItem(Long.parseLong("01001"));
+        bill.addItem(Long.parseLong("01001"));
+        processBill.printBill(bill.items);
     }
 }
